18612d1c51d9fb2be6cb58f2bdc08a2e92e7570f
jfohlmeisterp <julianapasold@gmail.com>
2020-07-27 13:00:17 -0300

    added

--- Program.cs
+++ Program.cs
@@ -7,30 +7,7 @@ namespace primeira_aula
     {
         static void Main(string[] args)
         {
-            // 6-Leia um array A com 12 elementos. Após sua leitura, colocar os seus elementos em
-            // ordem crescente. Depois ler um array B também com doze elementos, colocar os
-            // elementos de B em ordem decrescente. Construir um array C, onde cada elemento de C é a
-            // soma do elemento correspondente de A com b. Colocar em ordem crescente a matriz
-            // C e apresentar os seus valores.
-
-            int[] a = new int[12];
-            int[] b = new int[12];
-            var i = 0;
-
-            System.Console.WriteLine("Ordem crescente do array A:");
-            for (i = 0; i < 12; i++)
-            {
-                System.Console.WriteLine("Informe um número inteiro:");
-                a[i] = Convert.ToInt32(Console.ReadLine());
-            }
-
-            Array.Sort(a);
-
-            for (i = 0; i < 12; i++)
-            {
-                Console.WriteLine(a[i]); 
-            }
-
+            
         }
 
          static void Exercise1(string[] args)
@@ -607,6 +584,22 @@ namespace primeira_aula
 
         static void Array6(string[] args)
         {
+            // 6-Leia um array A com 12 elementos. Após sua leitura, colocar os seus elementos em
+            // ordem crescente. Depois ler um array B também com doze elementos, colocar os
+            // elementos de B em ordem decrescente. Construir um array C, onde cada elemento de C é a
+            // soma do elemento correspondente de A com b. Colocar em ordem crescente a matriz
+            // C e apresentar os seus valores.
+
+            int[] a = new int[12];
+            int[] b = new int[12];
+            var i = 0;
+
+            for (i = 0; i < 12; i++)
+            {
+                System.Console.WriteLine("Informe um número inteiro:");
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
         }
     }
 }
